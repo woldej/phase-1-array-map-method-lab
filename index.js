ca59f49a675525627cce38fb2f4e125bd0e73bdf
woldejamawolde <wolde.jama@m0ringaschool.com>
2023-10-15 23:12:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(title => {
+    return title.split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });}
